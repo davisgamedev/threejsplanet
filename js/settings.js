a464074dcefe02ecab127bd5689ebcafa86c6f33
David Smith <envative.dsmith@gmail.com>
2018-08-08 11:16:25 -0400

    optimizations

--- js/settings.js
+++ js/settings.js
@@ -2,8 +2,9 @@ var debugTexutures = true;
 var canvasSize = 2048;
 var pScale = 5;
 
-var optimizeNoise = true;
+var useOptimizedNoise = true;
 
+var generateNoiseTables = true;
+var generateNoiseTablesAmount = 10;
+var generateNoiseTablesAmountScaled = 3;
 var dataChannelsPerTable = 4;
-var generateNoiseTables = 10;
-var generateScaledNoiseTables = 4;
